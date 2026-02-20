5683855ceef18149dbdfd43c4e7fe9c3a833bbff
00000abc <claclof9@gmail.com>
2026-02-20 00:19:18 +0000

    Update build.sh

--- upload-backend/monprojet/build.sh
+++ upload-backend/monprojet/build.sh
@@ -4,10 +4,10 @@
 set -o errexit 
 
 # Installer les dépendances
-pip install -r /upload-backend/monprojet/requirements.txt
+pip install -r upload-backend/monprojet/requirements.txt
 
 # Collecter les fichiers statiques
-python /upload-backend/monprojet/manage.py collectstatic --no-input
+python upload-backend/monprojet/manage.py collectstatic --no-input
 
 # Faire les migrations
-python /upload-backend/monprojet/manage.py migrate
+python upload-backend/monprojet/manage.py migrate
